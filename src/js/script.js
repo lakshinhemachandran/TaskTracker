7afd28a51d817830d7a18b8a1c378ce4969384ea
lakshinhemachandran <lh140313@outlook.com>
2025-07-18 17:37:32 -0400

    Add localStorage - Will save tasks on reload - Added Delete Task Functionality - The task tracker app is DONE(for now)!

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,46 @@ function createNewTask() {
   const taskText = taskInput.value
 
   const li = document.createElement("li");
-  li.innerHTML = `${taskText} <button class="delete">Delete</button>`;
+  li.innerHTML = `<span class="task-test">${taskText} </span><button class="delete">Delete</button>`;
   taskList.appendChild(li);
   taskInput.value = "";
+  saveToLocalStorage();
 }
 
 addTaskBtn.addEventListener("click", createNewTask);
+
+function deleteTask(e) {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+  saveToLocalStorage();
+}
+
+taskList.addEventListener("click", deleteTask);
+
+
+function getAllTasks() {
+  const allTasks = document.querySelectorAll(".task-test");
+  const tasksTextArray = [...allTasks].map(task => task.textContent.trim());
+  return tasksTextArray;
+}
+
+function saveToLocalStorage() {
+  const tasksArray = getAllTasks();
+  const tasksString = JSON.stringify(tasksArray);
+  localStorage.setItem("tasks", tasksString);
+}
+window.onload = function() {
+  loadTasksFromStorage();
+};
+
+function loadTasksFromStorage() {
+  const savedTasksString = localStorage.getItem("tasks");
+  const savedTasksArray = savedTasksString ? JSON.parse(savedTasksString) : [];
+
+  savedTasksArray.forEach(taskText => {
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="task-test">${taskText}</span><button class="delete">Delete</button>`;
+    taskList.appendChild(li);
+  });
+}
